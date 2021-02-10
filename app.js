56009c783bc9b422a4a4874c0b63677cf95f8a5c
dougalcaleb <dougalcaleb@gmail.com>
2021-02-10 09:22:59 -0700

    setup

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+var postParse = bodyParser.urlencoded({extended: false});
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.set("view engine", "pug");
+app.set("views", "/views");
+app.use(express.static("public"));
+
+// =======================================
+// GET REQUESTS
+// =======================================
+
+app.get("/", (req, res) => {
+   app.render("index");
+});
+
+// =======================================
+// POST REQUESTS
+// =======================================
+
+app.post("/", postParse, (req, res) => {
+
+});
+
+// =======================================
+// MISC
+// =======================================
+
+app.listen(PORT, () => {
+   console.log(`Listening on port ${PORT}`);
+});
